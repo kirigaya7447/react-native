97c1d1283ec55e5ddfb9496ce60972017e812d11
kirigaya7447 <joaoh2915@gmail.com>
2023-12-13 11:06:20 -0300

    Consegui colocar ícones no bottom-tabs AEEEEEE

--- navigation/App.js
+++ navigation/App.js
@@ -2,6 +2,7 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
 import Contacts from "./src/pages/Contacts/index";
 import Informations from "./src/pages/Informations";
 import AppContacts from "./src/pages/AppContacts";
@@ -10,22 +11,22 @@ const navega = createStackNavigator();
 const botoesNav = createBottomTabNavigator();
 
 function TabNavigation() {
-  return (  
+  return (
     <botoesNav.Navigator>
-      <botoesNav.Screen name="AppContacts" component={AppContacts} />
-      <botoesNav.Screen name="Contacts" component={Contacts} />
+      <botoesNav.Screen name="AppContacts" component={AppContacts} options={{tabBarLabel: "Página Inicial", tabBarIcon: () => (<FontAwesome name="home" size={25} color="black"/>),}}/>
+      <botoesNav.Screen name="Contacts" component={Contacts} options={{tabBarLabel: "Contatos", tabBarIcon: () => (<FontAwesome name="coffee" size={23} color="black"/>),}}/>
     </botoesNav.Navigator>
   );
 }
 
 export default function App() {
   return (
-      <NavigationContainer>
-        <navega.Navigator>
-          <navega.Screen name="App" component={TabNavigation} />
-          <navega.Screen name="Informations" component={Informations} />
-        </navega.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <navega.Navigator>
+        <navega.Screen name="App" component={TabNavigation} />
+        <navega.Screen name="Informations" component={Informations} />
+      </navega.Navigator>
+    </NavigationContainer>
   );
 }
 
